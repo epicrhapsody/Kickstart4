918a68d9dfba03a41c2f5eb85cdd8f23c7bbcf8d
Karsten vom Wege <vomwege@gmail.com>
2018-09-23 20:15:16 +0200

    v4.0
    bugfix

--- js/main.js
+++ js/main.js
@@ -1,47 +1,33 @@
-$(document).ready(function(){
-
-    $(window).on('load resize', function(){
-        if ($(window).width() <= 899){
-            $('body').removeClass('desktop').addClass('mobile');
-        } else {
-            $('body').removeClass('mobile').addClass('desktop');
-        }
-    });
-
+$(window).on('load resize', function(){
+    if ($(window).width() <= 899){
+        $('body').removeClass('desktop').addClass('mobile');
+    } else {
+        $('body').removeClass('mobile').addClass('desktop');
+    }
+});
+$(document).ready(function () {
     $('#site').append('<div class="overlay hidden"></div>');
-   
-    $('.viewport').on('touch click', function(){
-      $(this).hide();
-    });
-
     $("nav#main .toggle").on('touch click', function(){
         $("nav#main .navigation,.overlay").toggleClass('hidden');
         $("nav#main .toggle").toggleClass('active');
     });
-
     $(".overlay").on('touch click', function(){
         $("nav#main .navigation,.overlay").addClass('hidden');
         $("nav#main .toggle").removeClass('active');
     });
-
     $('nav#main ul.menu').find('li:has(ul)').children('a').removeAttr('href').addClass("submenu");
     $('nav#main ul.menu li a.submenu').on('touch click', function(){
         $(this).toggleClass("active").closest('li').find('ul').toggleClass('active');
     });
-
     $("nav#main .navigation ul.menu").prepend('<div class="nav-header">Menu</div>');
     $('nav#main .navigation ul.menu li a.submenu').append('<span></span>');
-
-    
-
-     $(window).scroll(function(){
+    $(window).scroll(function(){
         if ($(this).scrollTop() > 100) {
             // do stuff
         } else {
             // do other stuff
         }
     });
-
     $("").on('touch click', function(){
         var $this = $("");
         if ($this.hasClass("inactive")) {
@@ -51,6 +37,4 @@ $(document).ready(function(){
             // do other stuff
         }
     });
-
  });
- 
