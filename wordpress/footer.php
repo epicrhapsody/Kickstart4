a26b386caefb07d413634d61024fb018a521d559
Karsten vom Wege <vomwege@gmail.com>
2019-02-14 15:31:08 +0100

    v4.4

--- wordpress/footer.php
+++ wordpress/footer.php
@@ -1,6 +1,6 @@
         </div><!--//#site-->
         <?php wp_footer(); ?>
-        <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/vendor/modernizr-3.6.0.min.js"></script>
+        <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/modernizr-3.6.0.min.js"></script>
         <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/script.min.js"></script>
     </body>
 </html>
